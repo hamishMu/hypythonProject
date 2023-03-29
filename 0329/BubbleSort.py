36644eacf2ef161f119fafceabc44e795dc681e4
Hamish Mu <731605632@qq.com>
2023-03-29 18:54:31 +0800

    增加了冒泡排序_python写法

---
+++ 0329/BubbleSort.py
@@ -0,0 +1,16 @@
+"""
+冒泡排序
+"""
+
+
+def bubble(nums):
+    for i in range(len(nums) - 1):
+        for j in range(len(nums) - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+
+if __name__ == '__main__':
+    nums = [3, 6, 4, 2, 11, 10, 5, 4, 3, 7]
+    print(bubble(nums))
