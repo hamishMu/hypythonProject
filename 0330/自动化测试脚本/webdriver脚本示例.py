1666183ddd56740a532cbc6c249708ab68bd985a
Hamish Mu <731605632@qq.com>
2023-03-30 15:25:03 +0800

    使用unitTest编写自动化示例脚本

---
+++ 0330/自动化测试脚本/webdriver脚本示例.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+______________________________
+  Author: muhuiyuan
+  Email : muhuiyuan@foxmail.com
+   Time : 2023/3/30 15:07
+    File: webdriver脚本示例.py
+Software: PyCharm
+______________________________
+"""
+
+from selenium import webdriver
+import  unittest
+
+class VisitPTPress(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+
+    def test_open_ptpress(self):
+        self.driver.get("https://www.ptpress.com.cn")
+        self.assertIn("图书",self.driver.page_source)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
